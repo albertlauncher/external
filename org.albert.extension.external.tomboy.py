f1d97798ab946a0d0f6e9cc38b9fe5d45de9ff95
Will Timpson <will.timpson@gmail.com>
2017-03-10 23:56:30 -0500

    Added a trigger, formatting tweaks

--- org.albert.extension.external.tomboy.py
+++ org.albert.extension.external.tomboy.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
 """ A quick and dirty shim to search Tomboy Notes over dbus.
-    Note: This does a full-text search on all of your notes for each keystroke, so you may want
-    to add a trigger if you have a lot of notes.
+    Note: This does a full-text search on all of your notes for each keystroke, so it may introduce
+    noticeable lag while searching if you have a lot of notes. As long as you use a trigger it
+    won't affect your non-Tomboy searches.
 """
 
 from pydbus import SessionBus
@@ -14,12 +15,12 @@ albert_op = os.environ.get("ALBERT_OP")
 
 if albert_op == "METADATA":
     metadata="""{
-      "iid":"org.albert.extension.external/v2.0",
-      "name":"Tomboy",
-      "version":"0.1",
-      "author":"Will Timpson",
-      "dependencies":["tomboy", "python-dbus"],
-      "trigger":""
+        "iid": "org.albert.extension.external/v2.0",
+        "name": "Tomboy",
+        "version": "0.1",
+        "author": "Will Timpson",
+        "dependencies": ["tomboy", "python-dbus"],
+        "trigger": "t "
     }"""
     print(metadata)
     sys.exit(0)
@@ -32,20 +33,20 @@ elif albert_op == "QUERY":
 
     bus = SessionBus()
     tomboy = bus.get("org.gnome.Tomboy", "/org/gnome/Tomboy/RemoteControl")
-    albert_query = os.environ.get("ALBERT_QUERY")
+    query = ' '.join(os.environ.get("ALBERT_QUERY").split(' ')[1:])
 
     items = []
-    for note in tomboy.SearchNotes(albert_query, False):
+    for note in tomboy.SearchNotes(query, False):
         item = {"id": note,
-                "name": tomboy.GetNoteTitle(note),
-                "Description": "Tomboy Note",
-                "icon": "tomboy",
-                "actions": [{
-                    "name": "Open Note",
-                    "command": "tomboy",
-                    "arguments": ["--open-note", note]
-                    }]
-                }
+            "name": tomboy.GetNoteTitle(note),
+            "Description": "Tomboy Note",
+            "icon": "tomboy",
+            "actions": [{
+                "name": "Open Note",
+                "command": "tomboy",
+                "arguments": ["--open-note", note]
+                }]
+            }
         items.append(item)
 
     print(json.dumps({"items": items}))
