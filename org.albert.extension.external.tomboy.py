4e213bab95cabfc12dd06f5f588bd8ae548685f8
Will Timpson <will.timpson@gmail.com>
2017-03-10 23:05:53 -0500

    Add Tomboy Notes search

---
+++ org.albert.extension.external.tomboy.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+""" A quick and dirty shim to search Tomboy Notes over dbus.
+    Note: This does a full-text search on all of your notes for each keystroke, so you may want
+    to add a trigger if you have a lot of notes.
+"""
+
+import dbus
+import os
+import sys
+import json
+
+albert_op = os.environ.get("ALBERT_OP")
+
+if albert_op == "METADATA":
+    metadata="""{
+      "iid":"org.albert.extension.external/v2.0",
+      "name":"Tomboy",
+      "version":"0.1",
+      "author":"Will Timpson",
+      "dependencies":["tomboy", "python-dbus"],
+      "trigger":""
+    }"""
+    print(metadata)
+    sys.exit(0)
+
+elif albert_op == "NAME":
+    print("Tomboy")
+    sys.exit(0)
+
+elif albert_op == "INITIALIZE":
+    sys.exit(0)
+
+elif albert_op == "FINALIZE":
+    sys.exit(0)
+
+elif albert_op == "SETUPSESSION":
+    sys.exit(0)
+
+elif albert_op == "SETUPSESSION":
+    sys.exit(0)
+
+elif albert_op == "TEARDOWNSESSION":
+    sys.exit(0)
+
+elif albert_op == "QUERY":
+
+    bus = dbus.SessionBus()
+    obj = bus.get_object("org.gnome.Tomboy", "/org/gnome/Tomboy/RemoteControl")
+    tomboy = dbus.Interface(obj, "org.gnome.Tomboy.RemoteControl")
+    albert_query = os.environ.get("ALBERT_QUERY")
+
+    items = []
+    for note in tomboy.SearchNotes(albert_query, False):
+        item = {}
+        item["id"] = note
+        item["name"] = tomboy.GetNoteTitle(note)
+        item["description"] = "Tomboy note"
+        item["icon"] = "tomboy"
+        item["actions"] = []
+
+        action = {}
+        action["name"] = "Open Note"
+        action["command"] = "tomboy"
+        action["arguments"] = ["--open-note", item["id"]]
+        item["actions"].append(action)
+
+        items.append(item)
+
+    print(json.dumps({"items": items}))
+    sys.exit(0)
