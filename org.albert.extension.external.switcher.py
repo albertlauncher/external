449b1a3b086bfc3ccc1c05fdeba4da116ac5157a
physalis02 <yosuke.otosu@gmail.com>
2017-08-11 14:20:06 +0900

    Add filtering and Remove unused parameters

--- org.albert.extension.external.switcher.py
+++ org.albert.extension.external.switcher.py
@@ -25,7 +25,7 @@ if albert_op == "METADATA":
       "version":"0.1",
       "author":"Airking05",
       "dependencies":[],
-      "trigger":":"
+      "trigger":","
     }
     print(json.dumps(metadata))
     sys.exit(0)
@@ -38,50 +38,56 @@ elif albert_op == "INITIALIZE":
     if not commands.getstatusoutput("which wmctrl")[0] is 0:
         sys.exit(1)
     sys.exit(0)
-    
+
 elif albert_op == "QUERY":
-    def build_action(name,interface,arguments=None):
+    def build_action(name):
         action = {
             "name": "switch to %s"%name,
             "command": "wmctrl",
-            "arguments": ["-a",arguments]
+            "arguments": ["-a",name]
         }
         return action
-   
+
     def build_item(id,name,icon,description,actions):
         item = {
             "id": id,
             "name": name,
             "description": description,
+            "completion": "switched",
             "icon": icon,
             "actions": actions
-            
         }
         return item
-    
-    albert_query = os.environ.get("ALBERT_QUERY")
+
+    albert_query = "".join(os.environ.get("ALBERT_QUERY").split(",")[1:])
     window_list = commands.getoutput("wmctrl -l -x").split("\n")
     items = []
+
     for window_row in window_list:
         action_list = []
         window = window_row.split()
-        config_file = APPLICATIONS_DIR + window[2].split(".")[0] + ".desktop"
-        if not os.path.exists(config_file):
-            name = window[2].split(".")[0]
-            icon = "utilities-terminal"
-            description = name
-        else:
-            parser = SafeConfigParser()
-            parser.read(config_file)
-            name = parser.get("Desktop Entry","Name")
-            icon = parser.get("Desktop Entry","Icon")
-            description = parser.get("Desktop Entry","Comment")
-        id = window[0]
-        action_list.append(build_action(name,False,name))
-        items.append(build_item(id,name,icon,description,action_list))
-        
+        window_program = window[2].split(".")[0]
+        window_title = "".join(window[4:])
+        config_file = APPLICATIONS_DIR + window_program + ".desktop"
+        if  albert_query == "" or (albert_query != "" and albert_query in window_program):
+            if not os.path.exists(config_file):
+                name = window_program
+                icon = "utilities-terminal"
+                description = window_title
+            else:
+                parser = SafeConfigParser()
+                parser.read(config_file)
+                name = parser.get("Desktop Entry","Name")
+                icon = parser.get("Desktop Entry","Icon")
+                description = window_title
+                # description = parser.get("Desktop Entry","Comment")
+
+            id = window[0]
+            action_list.append(build_action(name))
+            items.append(build_item(id,name,icon,description,action_list))
+
     print(json.dumps({"items":items}))
     sys.exit(0)
-    
+
 elif albert_op in ["FINALIZE", "SETUPSESSION", "TEARDOWNSESSION"]:
     sys.exit(0)
